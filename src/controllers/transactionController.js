43e8639fa3dc21151631446bbf8edd2f8240df38
Lisa-Shima <shimalisa2@gmail.com>
2025-02-26 09:20:24 +0200

    Committing - Transaction Management CRUDS: creating a withdraw transaction

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -21,4 +21,24 @@ const deposit = async(req, res) => {
     }
 }
 
-module.exports = {deposit}
+const withdraw = async(req, res) => {
+    const { account_id, amount} = req.body
+    try{
+        if(amount <= 0){
+            return res.status(400).json({message: 'Amount must be greater than zero'})
+        }
+
+        //Update balance
+        await pool.query('UPDATE accounts SET balance = balance - $1 WHERE account_number = $2', [amount, account_id])
+
+        //Create transaction
+        const transaction = await createTransaction(account_id, 'withdraw', amount)
+        res.status(200).json(transaction)
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json({message: 'Internal Server Error'})
+    }
+}
+
+module.exports = {deposit, withdraw}
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const router = express.Router()
-const {deposit} = require("../controllers/transactionController")
+const {deposit, withdraw} = require("../controllers/transactionController")
 const {protect} = require("../middlewares/auth")
 
-router.post('/', protect, deposit)
+router.post('/dep', protect, deposit)
+router.post('/with', protect, withdraw)
 
 module.exports = router
